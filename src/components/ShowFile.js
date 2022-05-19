1eaa3b979d6a64a5a2e96771639857346e5b8c2b
Abi Evans <abigailcevans@gmail.com>
2022-05-19 13:37:39 +0100

    highlights changed from canvas drawing to divs

--- src/components/ShowFile.js
+++ src/components/ShowFile.js
@@ -6,53 +6,58 @@ import { parse } from "side-lib";
 const ShowFile = ({fileContents}) => {
 
     const hiddenPre = useRef(null);
-    const [cWidth, setWidth] = useState("0px");
-    const [cHeight, setHeight] = useState("0px");
-
     const symptomCanvas = useRef(null);
     const sourceCode = useRef(null);
 
-    const symptoms = parse(fileContents).symptoms;
+    const [highlights, setHighlights] = useState([])
+
     const codeLines = fileContents.split(/\r?\n/);
 
-    useEffect(() => {
-        const sourceCodeStyle = getComputedStyle(sourceCode.current);
-        if (symptomCanvas.current.width !== sourceCodeStyle.width || symptomCanvas.current.height !== sourceCodeStyle.height) {
-            setWidth(getComputedStyle(sourceCode.current).width);
-            setHeight(getComputedStyle(sourceCode.current).height);
-        }
-    }, [fileContents, cWidth, cHeight, setWidth, setHeight]);
+
     // TODO: MAKE HIGHLIGHTS INTERACTIVE - may need to use divs rather than canvas
     useEffect(() => {
-        if (symptoms.length > 0) {
-            const ctx = symptomCanvas.current.getContext('2d');
-            ctx.font = getComputedStyle(hiddenPre.current).font;
-            ctx.fillStyle = "#49c8e7cc";
+        if (fileContents.length > 0 && highlights.length === 0) {
+            const symptoms = parse(fileContents).symptoms;
+            if (symptoms.length > 0) {
+                let highlightDivs = [];
 
-            let lineNumberStyle = getComputedStyle(document.getElementsByClassName("line-number")[0]);
-            let leftEdge = parseFloat(lineNumberStyle.width) + parseFloat(lineNumberStyle.marginRight);
-            let topEdge = parseFloat(getComputedStyle(sourceCode.current).marginTop);
-            let lineHeight = parseFloat(lineNumberStyle.height) + parseFloat(lineNumberStyle.marginTop);
-            for (let symptom of symptoms) {
-                let x = leftEdge;
-                if (symptom.lineIndex > 0) {
-                    x += ctx.measureText(codeLines[symptom.line].substring(0, symptom.lineIndex)).width; 
+                const ctx = symptomCanvas.current.getContext('2d');
+                ctx.font = getComputedStyle(hiddenPre.current).font;
+
+                let lineNumberStyle = getComputedStyle(document.getElementsByClassName("line-number")[0]);
+                let leftEdge = parseFloat(lineNumberStyle.width) + parseFloat(lineNumberStyle.marginRight);
+                let topEdge = parseFloat(getComputedStyle(sourceCode.current).marginTop);
+                let lineHeight = parseFloat(lineNumberStyle.height) + parseFloat(lineNumberStyle.marginTop);
+                for (let symptom of symptoms) {
+                    let x = leftEdge;
+                    if (symptom.lineIndex > 0) {
+                        x += ctx.measureText(codeLines[symptom.line].substring(0, symptom.lineIndex)).width; 
+                    }
+                    let y = topEdge + symptom.line * lineHeight;
+                    let lines = symptom.text.split(/\r?\n/);
+                    let w = ctx.measureText(codeLines[symptom.line].indexOf(lines[0]) >= 0 ? lines[0] : codeLines[symptom.line].substring(symptom.lineIndex, lines[0].length)).width; //parseFloat(getComputedStyle(hiddenPre.current).width);
+                    let h = lines.length * lineHeight;
+                    
+                    highlightDivs.push(
+                        <div className="highlight" key={highlightDivs}
+                            style={{left: `${x}px`, top: `${y}px`, width: `${w}px`, height:`${h}px`}}></div>
+                    )
+
                 }
-                let y = topEdge + symptom.line * lineHeight;
-                let lines = symptom.text.split(/\r?\n/);
-                let w = ctx.measureText(codeLines[symptom.line].indexOf(lines[0]) >= 0 ? lines[0] : codeLines[symptom.line].substring(symptom.lineIndex, lines[0].length)).width; //parseFloat(getComputedStyle(hiddenPre.current).width);
-                let h = lines.length * lineHeight;
-                ctx.beginPath();
-                ctx.rect(x, y, w, h);
-                ctx.fill();
+                setHighlights(highlightDivs);
             }
         }
-    }, [codeLines, symptoms, fileContents, cWidth, cHeight, setWidth, setHeight]);
+    }, [codeLines, fileContents, highlights]);
 
 
     return (
         <div className="code-container">
-            <canvas ref={symptomCanvas} id="symptom-canvas" width={cWidth} height={cHeight}></canvas>
+            <canvas ref={symptomCanvas} id="symptom-canvas"></canvas>
+            <div id="highlights">
+                {
+                    highlights
+                }
+            </div>
             <div ref={sourceCode} className="source-code">
                 {
                     codeLines.map((line, index) => 
@@ -72,8 +77,4 @@ export default ShowFile;
 
 ShowFile.propTypes = {
     fileContents: PropTypes.string
-}
-
-ShowFile.defaultProps = {
-    fileContents: "# Open a Python file using the button above"
 }
