8078e45a403225031c9c9621b858bf8de2644485
Abi Evans <abigailcevans@gmail.com>
2022-06-16 07:36:35 +0100

    close #11

--- src/redux/sourceReducer.js
+++ src/redux/sourceReducer.js
@@ -1,11 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { parse } from "side-lib";
+
+const processFilesAsync = async files => {
+    for (let file of files) {
+        file.analysis = parse(file.text);
+    }
+    return files;
+}
+
+const loadSourceFilesAsync = createAsyncThunk(
+    'source/loadSourceAsync',
+    async (files, thunkAPI) => {
+        const updatedFiles = await processFilesAsync(files);
+        return updatedFiles
+    }
+);
 
 const source = createSlice({
     name: 'source',
     initialState: {
         files: [],
-        activeFile: -1
+        activeFile: -1,
+        sourceLoading: false
     },
     reducers: {
         setSourceFiles: (state, action) => {
@@ -30,6 +46,21 @@ const source = createSlice({
             }
         }
     },
+    extraReducers: {
+        [loadSourceFilesAsync.pending]: (state) => {
+            state.sourceLoading = true;
+            state.files = [];
+            state.activeFile = -1;
+        },
+        [loadSourceFilesAsync.fulfilled]: (state, { payload }) => {
+            state.sourceLoading = false;
+            state.files = payload;
+            state.activeFile = payload.length > 0 ? 0 : -1;
+        },
+        [loadSourceFilesAsync.rejected]: (state) => {
+            state.sourceLoading = false
+        },
+    }
 });
 
 export default source.reducer;
