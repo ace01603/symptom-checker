e7c6b87c2feb416b41bc3e9070f5069221323044
Abi Evans <abigailcevans@gmail.com>
2022-06-20 10:55:57 +0100

    close #5

--- src/components/FileNavigation.js
+++ src/components/FileNavigation.js
@@ -1,11 +1,10 @@
 import { useDispatch, useSelector } from "react-redux";
-import { showNextFile, showPrevFile } from "../redux/sourceReducer";
+import { setActiveFile, showNextFile, showPrevFile } from "../redux/sourceReducer";
 
 const FileNavigation = () => {
     const INACTIVE_MSG = "Error! No file selected."
     const files = useSelector(state => state.source.files);
     const activeFile = useSelector(state => state.source.activeFile);
-    const fileName = activeFile >= 0 && files.length > 0 ? files[activeFile].fileName : INACTIVE_MSG;
 
     const btnClass = files.length === 0 ? "inactive" : "";
 
@@ -15,7 +14,20 @@ const FileNavigation = () => {
         <div className="results-container">
             <div className="file-navigation">
                 <button className={`step-button prev ${btnClass}`} onClick={() => dispatch(showPrevFile())}></button>
-                <div className="centre-align">{fileName}</div>
+                <div className="centre-align">
+                    {
+                        activeFile >= 0 ?
+                            <div className="select-container">
+                                <label htmlFor="file-selector" className="screenreader-only">Choose a file</label>
+                                <select className="file-select" name="file-selector" value={activeFile} onChange={e => dispatch(setActiveFile(e.target.value))}>
+                                    {
+                                        files.map((file, index) => <option key={index} value={index} >{file.fileName}</option>)
+                                    }
+                                </select>
+                            </div>
+                            : INACTIVE_MSG
+                    }
+                </div>
                 <button className={`step-button next ${btnClass}`} onClick={() => dispatch(showNextFile())}></button>
             </div>
             <div className="file-summary">
