837c2bd2de8a30b5db3ca9618d953c243e78196a
Abi Evans <abigailcevans@gmail.com>
2022-06-28 17:52:32 +0100

    close #22

--- src/components/ShowFile.js
+++ src/components/ShowFile.js
@@ -35,6 +35,7 @@ const ShowFile = () => {
             for (let symptom of symptoms) {
                 let x = ctx.measureText(codeLines[symptom.line].substring(0, symptom.lineIndex).replace("\t", "    ")).width;
                 let y = symptom.line * lineHeight;
+                // Ignore \n in string literal
                 let lines = symptom.text.replace("\\n","  ").split(/\\|\r?\n/); // split on Python continuation symbol as well as line breaks
                 let w = ctx.measureText(symptom.text).width;
                 let h = lines.length * lineHeight;                
