d41b942af1f98ba5126e387ed343e014ce176655
Abi Evans <50892754+ace01603@users.noreply.github.com>
2025-01-29 17:59:31 +0000

    updated build settings to work with vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://ace01603.github.io/symptom-checker/",
+    build: {
+        outDir: 'docs',
+        emptyOutDir: true, // also necessary
+    }
 })
